73317937f9ee2403b82fb795bedbd162a89c5805
linsinrou <101238809+linsinrou@users.noreply.github.com>
2022-11-24 23:29:47 +0800

    Update Source.cpp

--- 2-10/Source.cpp
+++ 2-10/Source.cpp
@@ -44,3 +44,29 @@ int main(int argc, char* argv[]) {
     matrix_mul(A, B, C);
     matrix_print(C);
 }
+
+/*
+請寫一個void matrix_mul(int a[][columns], int b[][columns], int c[][columns])的副程式，可以對矩陣a和矩陣b進行「矩陣相乘」，並將相乘的結果儲存至矩陣c。再寫void matrix_print(int arr[][columns])的副程式，輸出矩陣c的結果。
+
+矩陣a範例：
+1 2 3
+4 5 6
+
+矩陣b範例：
+1 0 1
+1 1 0
+0 1 1
+
+相乘後的矩陣c：
+3 5 4
+9 11 10
+
+輸出範例：
+3 5 4 9 11 10
+
+
+
+系統輸入：15個int型態的數值（2*3的矩陣和3*3的矩陣）
+
+結果輸出：6個int型態的數值
+*/
